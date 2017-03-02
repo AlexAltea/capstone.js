fbaa14b44b2730fcad08200a28716d566be70e6c
Yury Delendik <ydelendik@mozilla.com>
2017-03-02 08:11:53 -0600

    Allows to use the library file as UMD module.

--- src/capstone-wrapper.js
+++ src/capstone-wrapper.js
@@ -259,3 +259,10 @@ var cs = {
         }
     },
 };
+
+// Exports for AMD and CommonJS
+if (typeof define === 'function' && define.amd) {
+    define([], function () { return cs; });
+} else if (typeof module === 'object' && module.exports) {
+    module.exports = cs;
+}
