128590b6ecac6d3f1c060b0459ce90fb884be7be
paulwuertz <paulwuertz@posteo.de>
2026-06-14 16:21:23 +0200

    Update build.py script to Emscripten 5.0.7

--- build.py
+++ build.py
@@ -3,7 +3,7 @@
 # INFORMATION:
 # This scripts compiles the original Capstone framework to JavaScript
 
-from __future__ import print_function
+
 import os
 import re
 import sys
@@ -24,7 +24,7 @@ EXPORTED_FUNCTIONS = [
     '_cs_support',
     '_cs_version',
     '_cs_strerror',
-    '_cs_disasm_ex',
+    '_cs_disasm',
     '_cs_disasm_iter',
     '_cs_malloc',
     '_cs_reg_read',
@@ -36,16 +36,28 @@ EXPORTED_FUNCTIONS = [
 EXPORTED_CONSTANTS = [
     'bindings/python/capstone/arm64_const.py',
     'bindings/python/capstone/arm_const.py',
+    "bindings/python/capstone/bpf_const.py",
+    'bindings/python/capstone/evm_const.py',
+    'bindings/python/capstone/m680x_const.py',
+    'bindings/python/capstone/m68k_const.py',
     'bindings/python/capstone/mips_const.py',
+    "bindings/python/capstone/mos65xx_const.py",
     'bindings/python/capstone/ppc_const.py',
+    "bindings/python/capstone/riscv_const.py",
+    "bindings/python/capstone/sh_const.py",
     'bindings/python/capstone/sparc_const.py',
     'bindings/python/capstone/sysz_const.py',
+    'bindings/python/capstone/tms320c64x_const.py',
+    "bindings/python/capstone/tricore_const.py",
+    "bindings/python/capstone/wasm_const.py",
     'bindings/python/capstone/x86_const.py',
     'bindings/python/capstone/xcore_const.py',
 ]
 
 AVAILABLE_TARGETS = [
-    'ARM', 'ARM64', 'MIPS', 'PPC', 'SPARC', 'SYSZ', 'XCORE', 'X86'
+    'ARM', 'ARM64', 'MIPS', 'PPC', 'SPARC', 'SYSZ', 'XCORE', 'X86',
+    'M68K', 'TMS320C64X', 'M680X', 'EVM', 'MOS65XX', 'WASM', 'BPF',
+    'RISCV', 'SH', 'TRICORE'
 ]
 
 # Directories
@@ -57,8 +69,8 @@ def generateConstants():
         path = os.path.join(CAPSTONE_DIR, path)
         with open(path, 'r') as f:
             code = f.read()
-            code = re.sub('\n([^#\t\r\n ])', '\ncs.\g<1>', code)
-            code = re.sub('(.* = [A-Za-z_])', '# \g<1>', code)
+            code = re.sub(r'\n([^#\t\r\n ])',  r'\ncs.\g<1>', code)
+            code = re.sub(r'(.* = [A-Za-z_])', r'# \g<1>', code)
             code = code.replace('#', '//')
         out.write(code)
     out.close()
@@ -71,14 +83,15 @@ def compileCapstone(targets):
         pass
 
     # CMake
-    cmd = 'cmake'
-    cmd += os.path.expandvars(' -DCMAKE_TOOLCHAIN_FILE=$EMSCRIPTEN/cmake/Modules/Platform/Emscripten.cmake')
+    cmd = 'emcmake cmake'
+    cmd += ' -DCMAKE_TOOLCHAIN_FILE=/usr/lib/emscripten/cmake/Modules/Platform/Emscripten.cmake'
     cmd += ' -DCMAKE_BUILD_TYPE=Release'
-    cmd += ' -DCMAKE_C_FLAGS=\"-Wno-warn-absolute-paths\"'
+    cmd += ' -DCMAKE_C_FLAGS="-Wno-warn-absolute-paths"'
     cmd += ' -DCAPSTONE_BUILD_TESTS=OFF'
+    cmd += ' -DCAPSTONE_BUILD_STATIC_LIBS=ON'
     cmd += ' -DCAPSTONE_BUILD_SHARED=OFF'
     if targets:
-        targets = map(lambda t: t.upper(), targets)
+        targets = [t.upper() for t in targets]
         for arch in AVAILABLE_TARGETS:
             if arch not in targets:
                 cmd += ' -DCAPSTONE_%s_SUPPORT=0' % arch
@@ -96,32 +109,32 @@ def compileCapstone(targets):
     if os.name == 'nt':
         make = 'mingw32-make'
     if os.name == 'posix':
-        make = 'make'
+        make = 'emmake make'
     if os.system(make) != 0:
         print("Make errored")
         sys.exit(1)
     os.chdir('..')
 
     # Compile static library to JavaScript
-    exports = EXPORTED_FUNCTIONS[:]
+    exports = EXPORTED_FUNCTIONS
     methods = [
         'ccall', 'getValue', 'setValue', 'writeArrayToMemory', 'UTF8ToString'
     ]
-    cmd = os.path.expandvars('$EMSCRIPTEN/emcc')
-    cmd += ' -Os --memory-init-file 0'
+    cmd = 'emcc'
+    cmd += ' -Os'
     cmd += ' capstone/libcapstone.a'
     cmd += ' -s EXPORTED_FUNCTIONS=\"[\''+ '\', \''.join(exports) +'\']\"'
-    cmd += ' -s EXTRA_EXPORTED_RUNTIME_METHODS=\"[\''+ '\', \''.join(methods) +'\']\"'
+    cmd += ' -s EXPORTED_RUNTIME_METHODS=\"[\''+ '\', \''.join(methods) +'\']\"'
     cmd += ' -s ALLOW_MEMORY_GROWTH=1'
     cmd += ' -s MODULARIZE=1'
-    cmd += ' -s WASM=0'
+    cmd += ' -s WASM=2'
     cmd += ' -s EXPORT_NAME="\'MCapstone\'"'
     if targets:
         cmd += ' -o src/libcapstone-%s.out.js' % '-'.join(targets).lower()
     else:
         cmd += ' -o src/libcapstone.out.js'
     if os.system(cmd) != 0:
-        print("Emscripten errored")
+        print("Emscripten errored", cmd)
         sys.exit(1)
 
 
